d761bc91796f2f4e932c98fb5bca45f3fd9154fd
wangguanwu <2531507093@qq.com>
2022-09-21 17:26:07 +0800

    first commit

---
+++ seata-xa/xa-order-service/src/main/java/com/gw/seata/config/FeignConfig.java
@@ -0,0 +1,18 @@
+package com.gw.seata.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author guanwu
+ * @created on 2022-09-21 16:58:26
+ **/
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
